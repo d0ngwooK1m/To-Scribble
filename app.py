07f2a020226961cfe219f061bf41f29882e168ed
unknown <lsj2867@gmail.com>
2021-09-15 10:00:16 +0900

    view api 추가

--- app.py
+++ app.py
@@ -2,8 +2,10 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 from pymongo import MongoClient
-
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.lsjtest                      # 'dbsparta'라는 이름의 db를 만듭니다.
 app = Flask(__name__)
+
 
 # 페이지 연결기능 여기서 작성해주세요!
 @app.route('/')
@@ -38,6 +40,19 @@ def delete_mypage():
     # postId_receive = request.
     # db.mypage.delete_one({'name': postId_receive})
     return jsonify({'msg': '삭제 완료!'})
+
+@app.route('/mypage/showmypost', methods=['GET'])
+def show_mypost():
+    sample_receive = request.args.get('sample_give')
+    my_posts = list(db.posts.find({'num': sample_receive}, {'_id': False}))
+    print(my_posts)
+    return jsonify({'my_posts': my_posts})
+
+
+@app.route('/allpost', methods=['GET'])
+def read_reviews():
+    all_post = list(db.posts.find({}, {'_id': False}))
+    return jsonify({'all_post': all_post})
 
 
 if __name__ == '__main__':
--- static/js/mainpage.js
+++ static/js/mainpage.js
@@ -1,3 +1,6 @@
+$(document).ready(function () {
+                showAllpost();
+            });
 let checkOpened = false;
 const popupBtn = document.querySelector('.drawing-hide-button');
 const testBox = document.querySelector('.diary');
@@ -11,3 +14,41 @@ popupBtn.addEventListener('click', () => {
         testBox.style.display = "none";
     }
 });
+function showAllpost() {
+                $.ajax({
+                    type: "GET",
+                    url: "/allpost",
+                    data: {},
+                    success: function (response) {
+                        let posts = response['all_post']
+                        console.log(posts)
+
+                            for (let i = 0; i < posts.length; i++) {
+                                let imageurl = posts[i]['image']
+                                let text = posts[i]['text']
+                                let date = posts[i]['date']
+                                let weather = posts[i]['weather']
+
+                                let temp_html = `
+                            <div class="card">
+                                <div class="card-image">
+                                    <figure class="image is-4by3">
+                                        <img src=${imageurl} >
+                                    </figure>
+                                </div>
+                                <div class="card-content">
+                                    <div class="content">
+                                        ${text}
+                                        <br>
+                                        <time datetime="2016-1-1">${date}</time>
+                                        <br>
+                                        <weather>${weather}</weather>
+                                    </div>
+                                </div>
+                            </div>
+                            `
+                                $('#card-box').append(temp_html)
+                            }
+                    }
+                })
+            }
